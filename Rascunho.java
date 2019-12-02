b665034146b6da9d30687260a7391bdfb5e6a9b5
SlackxRA1D <53799083+SlackxRA1D@users.noreply.github.com>
2019-12-02 20:15:44 -0300

    Add files via upload

---
+++ Rascunho.java
@@ -0,0 +1,24 @@
+public class Rascunho {
+    int parte =0;
+   if (parte == 0) {
+    textos.setText();
+    op1.setText("Cadastrar");
+    op2.setVisible(false);
+    op3.setText("Exibir");
+}    
+   else if(parte == 1){
+    textos.setText();
+    op1.setText("Arqueiro");
+    op2.setText("Guerreiro");
+    op3.setText("Mago");
+}
+   else if (parte == 2){
+    textos.setText();
+    op1.setText("Modificar");
+    op2.setText("Excluir");
+    op3.setText("Voltar");
+      
+}
+   
+    
+}
